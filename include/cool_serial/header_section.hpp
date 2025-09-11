6cf2004225094dc4f33265e740e131eee920df67
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-09-11 13:46:01 -0500

    feat(HeaderSection): Organize sections into serializable chunks

--- include/cool_serial/header_section.hpp
+++ include/cool_serial/header_section.hpp
@@ -2,25 +2,46 @@
 #define COOL_SERIAL_HEADER_SECTION_HPP
 
 #include "byte.hpp"
+#include "cppcrc/cppcrc.h"
 #include <cstdint>
 
 namespace coolSerial
 {
     constexpr Byte kStartOfFrame{137}; // Approximate fine structure constant :D
-    
+
+    /**
+     * This is the portion of the header that determines how the data section
+     * is interpreted.
+     *
+     * @param dataLength - how many bytes make up the data section
+     * @param dataType - how the data should be interpreted (user defined)
+     */
+    struct DataInfo
+    {
+        std::uint16_t dataLength;
+        std::uint8_t dataType;
+    };
+
     /**
      * Defines the header structure. The SOF byte is not considered part of the
      * header section.
      *
-     * @param dataLength - how many bytes make up the data section
-     * @param messageType - how the data should be interpreted (user defined)
-     * @param headerCrc - CRC8 for the header section (minus the CRC8)
+     * @param dataInfo - information about the data section
+     * @param headerCrc - CRC8 for dataInfo (minus the CRC8)
+     *
+     * This is a struct for easier serialization and deserialization
      */
-    struct HeaderSection
+    struct HeaderData
     {
-        std::uint16_t dataLength;
-        std::uint8_t messageType;
-        Byte headerCrc; 
+        DataInfo dataInfo;
+        Byte dataInfoCrc; 
+    };
+
+    class HeaderSection
+    {
+    public:
+        HeaderSection(uint8_t messageType, uint16_t dataLength);
+    private:
     };
 
 };
