1682840f239c3be74dfbddadd5c6a392aeb17bd3
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-09-11 12:36:25 -0500

    test(lib): Ensure that cista can serialize arbitrary fixed-length structs

---
+++ test/cistaTest.cpp
@@ -0,0 +1,38 @@
+#include "cista/cista.h"
+#include <gtest/gtest.h>
+
+#include <vector>
+
+TEST(barebones, cista)
+{
+    struct BasicStruct
+    {
+        int integer;
+        double x;
+        double y;
+        double z;
+    };
+
+    BasicStruct input
+    {
+        .integer = 123234,
+        .x = 0.234234,
+        .y = -2352.2342,
+        .z = 0.345345
+    };
+
+
+
+    std::vector<unsigned char> bytes{cista::serialize(input)};
+
+    BasicStruct output{*cista::deserialize<BasicStruct>(bytes)};
+
+    for (auto& byte : bytes)
+    {
+        std::cout << ", ";
+        std::cout << std::hex << int(byte);
+
+    }
+
+    EXPECT_EQ(output.integer, input.integer);
+}
