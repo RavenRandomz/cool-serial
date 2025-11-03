cbcdbd2383deda5fa33308ccbac6297c29b1e4f2
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-03 12:52:54 -0600

    feat: Allow other objects to be informed of a found start of frame

---
+++ include/cool_serial/dynamic_parser/start_of_frame_found_listener.hpp
@@ -0,0 +1,11 @@
+#ifndef COOL_SERIAL_DYNAMIC_PARSER_START_OF_FRAME_FOUND_LISTENER_HPP
+#define COOL_SERIAL_DYNAMIC_PARSER_START_OF_FRAME_FOUND_LISTENER_HPP
+namespace coolSerial
+{
+    class StartOfFrameFoundListener
+    {
+    public:
+        virtual void startofFrameFound() = 0;
+    };
+}
+#endif
