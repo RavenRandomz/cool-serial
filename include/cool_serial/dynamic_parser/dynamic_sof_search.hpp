181759e625ca7a4f22a453c821fc41441e2fabcf
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-03 13:08:49 -0600

    refactor: Create testable version of SOF search
    Make continuousParser much more legible and tested

---
+++ include/cool_serial/dynamic_parser/dynamic_sof_search.hpp
@@ -0,0 +1,57 @@
+#ifndef COOL_SERIAL_DYNAMIC_PASER_DYNAMIC_SOF_SEARCH_HPP
+#define COOL_SERIAL_DYNAMIC_PASER_DYNAMIC_SOF_SEARCH_HPP
+
+#include "cool_serial/byte_queue.hpp"
+#include "cool_serial/cool_message.hpp"
+#include "cool_serial/dynamic_parser/start_of_frame_found_listener.hpp"
+
+namespace coolSerial
+{
+    /**
+     * This can continuously search for the coolSerial SOF
+     * Upon finding the SOF, it informs a single listener.
+     *
+     * Every time update() is called, it checks all available
+     * bytes in the queue until the SOF is located. No more bytes
+     * will be popped otherwise.
+     *
+     * Due to performance reasons, there is only one listener.
+     * A listener adapter can be informed which would them pass
+     * the call towards a collection of listeners if need be.
+     */
+    class DynamicSofSearch
+    {
+    public:
+        DynamicSofSearch(
+            ByteQueue& queue,
+            StartOfFrameFoundListener& listener
+        ):
+            queue_{queue},
+            frameFoundListener_{listener}
+            
+        {
+        }
+
+        void update()
+        {
+            //Consider having an upper limit
+            while(queue_.byteAvailable())
+            {
+                sofListenerInformCheck();
+            }
+        }
+    private:
+        ByteQueue& queue_;
+        StartOfFrameFoundListener& frameFoundListener_;
+
+        void sofListenerInformCheck()
+        {
+            const Byte kNext{queue_.getNextPoppedByte()};
+            if(kNext == CoolMessage::kStartOfFrame)
+            {
+                frameFoundListener_.startofFrameFound();
+            }
+        }
+    };
+}
+#endif
