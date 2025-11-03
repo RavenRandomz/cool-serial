6ec69fac0333803db91b0bfc404c96f44d99356d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-03 12:52:21 -0600

    feat: Initialize api with generic stub

---
+++ include/cool_serial/dynamic_parser/dynamic_parser.hpp
@@ -0,0 +1,21 @@
+#ifndef COOL_SERIAL_DYNAMIC_PARSER_DYNAMIC_PARSER_HPP
+#define COOL_SERIAL_DYNAMIC_PARSER_DYNAMIC_PARSER_HPP
+namespace coolSerial
+{
+    /**
+     * This is able to parse an incomplete
+     * stream of bytes (varying chunks)
+     * When a complete Cool Serial message
+     * has been recovered.
+     *
+     * Listeners can be attached.
+     */
+    class DynamicParser
+    {
+    public:
+         void update()
+        {
+        }
+    };
+};
+#endif
