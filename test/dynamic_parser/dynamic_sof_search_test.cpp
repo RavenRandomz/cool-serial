80db2698fabea03a9597208d61d1c74ae6980afa
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-03 17:00:09 -0600

    test: Ensure start of frame is actually found

--- test/dynamic_parser/dynamic_sof_search_test.cpp
+++ test/dynamic_parser/dynamic_sof_search_test.cpp
@@ -21,4 +21,55 @@ TEST(DynamicSofSearch, noSof)
     DynamicSofSearch sofSearcher{testQueue, listener};
     sofSearcher.update();
 }
+
+/**
+ * The test byte queue will contain the SOF 0x84
+ */
+TEST(DynamicSofSearch, sofBegin)
+{
+    // This is adapt from the original continuousParser test since it has a known precalculated value
+    Bytes finalHalfOfData{0x84,0x27, 0xa0, 0xa8, 0xc8, 0x1b, 0xf5, 0x10, 0xd, 0xeb, 0xdc, 0xe0, 0x40};
+    ByteQueue testQueue{std::deque<Byte>{finalHalfOfData.begin(), finalHalfOfData.end()}};
+
+    // Fail if listening function is called
+    testing::StrictMock<StartOfFrameFoundListenerMock> listener{};
+
+    EXPECT_CALL(listener, startOfFrameFound());
+    DynamicSofSearch sofSearcher{testQueue, listener};
+    sofSearcher.update();
+}
+
+/**
+ * The test byte queue will contain the SOF 0x84
+ */
+TEST(DynamicSofSearch, sofMid)
+{
+    // This is adapted from the original continuousParser test since it has a known precalculated value
+    Bytes finalHalfOfData{0x27, 0xa0, 0xa8, 0x84, 0x1b, 0xf5, 0x10, 0xd, 0xeb, 0xdc, 0xe0, 0x40};
+    ByteQueue testQueue{std::deque<Byte>{finalHalfOfData.begin(), finalHalfOfData.end()}};
+
+    // Fail if listening function is called
+    testing::StrictMock<StartOfFrameFoundListenerMock> listener{};
+
+    EXPECT_CALL(listener, startOfFrameFound());
+    DynamicSofSearch sofSearcher{testQueue, listener};
+    sofSearcher.update();
+}
+
+/**
+ * The test byte queue will contain the SOF 0x84
+ */
+TEST(DynamicSofSearch, sofEnd)
+{
+    // This is adapted from the original continuousParser test since it has a known precalculated value
+    Bytes finalHalfOfData{0x27, 0xa0, 0xa8, 0x82, 0x1b, 0xf5, 0x10, 0xd, 0xeb, 0xdc, 0xe0, 0x84};
+    ByteQueue testQueue{std::deque<Byte>{finalHalfOfData.begin(), finalHalfOfData.end()}};
+
+    // Fail if listening function is called
+    testing::StrictMock<StartOfFrameFoundListenerMock> listener{};
+
+    EXPECT_CALL(listener, startOfFrameFound());
+    DynamicSofSearch sofSearcher{testQueue, listener};
+    sofSearcher.update();
+}
 }
