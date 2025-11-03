3235fcbc9c0a8df99fef1f81b05d16b60bb2be49
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-03 16:48:58 -0600

    test: Make sure that no false positive for SOF occurs

---
+++ test/dynamic_parser/dynamic_sof_search_test.cpp
@@ -0,0 +1,24 @@
+#include "cool_serial/byte_queue.hpp"
+#include "cool_serial/dynamic_parser/dynamic_sof_search.hpp"
+#include "cool_serial/mock/dynamic_parser/start_of_frame_listener_mock.hpp"
+
+#include <gtest/gtest.h>
+
+namespace coolSerial::dynamicSofSearchTest
+{
+/**
+ * The test byte queue will NOT contain the SOF 0x84
+ */
+TEST(DynamicSofSearch, noSof)
+{
+    // This is taken from the original continuousParser test since it has a known precalculated value
+    Bytes finalHalfOfData{0x84,0x27, 0xa0, 0xa8, 0xc8, 0x1b, 0xf5, 0x10, 0xd, 0xeb, 0xdc, 0xe0, 0x40};
+    ByteQueue testQueue{std::deque<Byte>{finalHalfOfData.begin(), finalHalfOfData.end()}};
+
+    // Fail if listening function is called
+    testing::StrictMock<StartOfFrameFoundListenerMock> listener{};
+
+    DynamicSofSearch sofSearcher{testQueue, listener};
+    sofSearcher.update();
+}
+}
