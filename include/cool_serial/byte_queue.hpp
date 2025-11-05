d547f41902b09d59e80d984a9d9ec7d0e183fbe1
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-05 08:49:15 -0600

    feat: Add convenient method to add a collection

--- include/cool_serial/byte_queue.hpp
+++ include/cool_serial/byte_queue.hpp
@@ -2,6 +2,8 @@
 #define COOL_SERIAL_BYTE_QUEUE
 
 #include "byte.hpp"
+#include "bytes.hpp"
+
 #include <queue>
 
 namespace coolSerial
@@ -21,6 +23,14 @@ namespace coolSerial
             const Byte kNext{front()};
             pop();
             return kNext;
+        }
+
+        void addBytes(const Bytes& bytes)
+        {
+            for(const Byte& kByte : bytes)
+            {
+                push(kByte);
+            }
         }
     };
 }
