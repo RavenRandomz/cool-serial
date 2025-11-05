eae11ce3c1abd0ccd62ce8e90922a6d7388f502b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-05 15:49:30 -0600

    feat: Reduce code duplication
    Length based buffer extration is utilized by both the header nd data extracters

---
+++ include/cool_serial/dynamic_parser/dynamic_segment_extractor.hpp
@@ -0,0 +1,80 @@
+#ifndef COOL_SERIAL_DYNAMIC_PARSER_DYNAMIC_SEGMEN_EXTRACTOR
+#define COOL_SERIAL_DYNAMIC_PARSER_DYNAMIC_SEGMEN_EXTRACTOR
+
+#include "cool_serial/byte_queue.hpp"
+#include "cool_serial/dynamic_parser/segment_found_listener.hpp"
+
+namespace coolSerial
+{
+/**
+ * This continuously checks a stream gathering data until
+ * it has obtained a number of bytes equal to the segmentSize
+ *
+ * update() must be repeatedly called.
+ *
+ * WARNING: This causes the byteQueue to dump each byte. This is a destructive
+ * operation
+ */
+class DynamicSegmentExtractor
+{
+public:
+    DynamicSegmentExtractor(ByteQueue& queue, SegmentFoundListener& listener, int segmentSize)
+        :
+        queue_{queue},
+        listener_{listener},
+        kMaxSegmentIndex_{segmentSize - 1} // Index starts at zero
+    {
+        segmentBytes_.reserve(segmentSize);
+    }
+
+    /**
+     * This will take all of the bytes within the queue
+     * and attempt to extract a segment of the specified size.
+     *
+     * If the segment is found, the queue will not be completely emptied.
+     */
+    void update()
+    {
+        // The gist is that it sequentially takes
+        // a byte into the segment buffer until
+        // the segment buffer is full.
+        //
+        // The listener will be have their segmentFound function
+        // called which will pass the information.
+        while(queue_.byteAvailable())
+        {
+            // This is to optimize performance
+            // So instead of using a queue, it is directly written
+            // to the array
+            segmentBytes_[extractedIndex_] = queue_.getNextPoppedByte();
+
+            if(extractedIndex_ == kMaxSegmentIndex_)
+            {
+                listener_.segmentFound(segmentBytes_);
+                resetExtraction();
+                break;
+            }
+            // Prepare next interation for next index
+            ++extractedIndex_;
+        }
+    }
+
+    /**
+     * The current message buffer will be overwritten
+     * with the next extraction.
+     */
+    void resetExtraction()
+    {
+        extractedIndex_ = 0;
+    }
+
+private:
+    ByteQueue& queue_; 
+    SegmentFoundListener& listener_;
+    const int kMaxSegmentIndex_;
+
+    Bytes segmentBytes_{};
+    int extractedIndex_{0};
+};
+}
+#endif
