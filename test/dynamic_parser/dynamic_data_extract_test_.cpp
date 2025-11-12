1e1676b6746ae42562c8b078ace863bc40cf794f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-12 16:57:21 -0600

    test: Ensure proper functioning

---
+++ test/dynamic_parser/dynamic_data_extract_test_.cpp
@@ -0,0 +1,53 @@
+#include "cool_serial/byte_queue.hpp"
+#include "cool_serial/dynamic_parser/dynamic_data_extract.hpp"
+#include "cool_serial/mock/dynamic_parser/data_found_listener_mock.hpp"
+
+#include "gmock/gmock.h"
+#include <cool_serial/cool_message.hpp>
+#include <gtest/gtest.h>
+
+namespace coolSerial::dynamicDataExtractTest
+{
+/**
+ * Test for no false positives regarding empty queue
+ */
+TEST(DynamicDataExtract, emptyQueueNoFalsePositive)
+{
+    ByteQueue testQueue{};
+
+    // Fail if listening function is called
+    testing::StrictMock<DataFoundListenerMock> listener{};
+
+    const int kDataSize{5};
+    const Byte kDataType{0};
+    const DataInfo kDataInfo{kDataSize,kDataType};
+
+    DynamicDataExtract extractor{testQueue, listener};
+    extractor.setDataExtractionInfo(kDataInfo);
+    extractor.update();
+    // The listener should not be called
+}
+
+/**
+ * Test for successful extraction with a buffer that has an equal amount to the desired
+ * size
+ */
+TEST(DynamicDataExtract, basicExtraction)
+{
+    const Bytes kTestBytes{1,2,3,4,5};
+    const std::uint16_t kDataSize{static_cast<uint16_t>(kTestBytes.size())};
+    ByteQueue queue{};
+    queue.addBytes(kTestBytes);
+
+    const Byte kDataType{0};
+    const DataInfo kDataInfo{kDataSize,kDataType};
+    testing::StrictMock<DataFoundListenerMock> listener{};
+
+    DynamicDataExtract extractor{queue, listener};
+    extractor.setDataExtractionInfo(kDataInfo);
+
+    const CoolMessageData kExpectedMessageData{kDataType, kTestBytes};
+    EXPECT_CALL(listener, dataFound(kExpectedMessageData));
+    extractor.update();
+}
+}
