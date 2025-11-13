f605b450fab97ac9c0522f3476a1567363a44be5
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-12 20:20:00 -0600

    test: Ensure extraction of only relevant data

--- test/dynamic_parser/dynamic_data_extract_test_.cpp
+++ test/dynamic_parser/dynamic_data_extract_test_.cpp
@@ -50,4 +50,56 @@ TEST(DynamicDataExtract, basicExtraction)
     EXPECT_CALL(listener, dataFound(kExpectedMessageData));
     extractor.update();
 }
+
+/**
+ * Test for successful extraction with a buffer that has an equal amount to the desired
+ * size
+ */
+TEST(DynamicDataExtract, extraExtraction)
+{
+    const Bytes kTestBytes{1,2,3,4,5};
+    const Bytes kTestQueue{1,2,3,4,5, 2, 4, 5};
+    const std::uint16_t kDataSize{static_cast<uint16_t>(kTestBytes.size())};
+    ByteQueue queue{};
+    queue.addBytes(kTestQueue);
+
+    const Byte kDataType{0};
+    const DataInfo kDataInfo{kDataSize,kDataType};
+    testing::StrictMock<DataFoundListenerMock> listener{};
+
+    DynamicDataExtract extractor{queue, listener};
+    extractor.setDataExtractionInfo(kDataInfo);
+
+    const CoolMessageData kExpectedMessageData{kDataType, kTestBytes};
+    EXPECT_CALL(listener, dataFound(kExpectedMessageData));
+    extractor.update();
+}
+
+/**
+ * Test for successful extraction with a buffer that has an equal amount to the desired
+ * size
+ */
+TEST(DynamicDataExtract, partialExtraction)
+{
+    const Bytes kTestBytes{1,2,3,4,5};
+    const Bytes kTestBytes1{1,2};
+    const Bytes kTestBytes2{3,4,5};
+    const std::uint16_t kDataSize{static_cast<uint16_t>(kTestBytes.size())};
+    ByteQueue queue{};
+    queue.addBytes(kTestBytes1);
+
+    const Byte kDataType{0};
+    const DataInfo kDataInfo{kDataSize,kDataType};
+    testing::StrictMock<DataFoundListenerMock> listener{};
+
+    DynamicDataExtract extractor{queue, listener};
+    extractor.setDataExtractionInfo(kDataInfo);
+
+    const CoolMessageData kExpectedMessageData{kDataType, kTestBytes};
+    extractor.update();
+
+    EXPECT_CALL(listener, dataFound(kExpectedMessageData));
+    queue.addBytes(kTestBytes2);
+    extractor.update();
+}
 }
