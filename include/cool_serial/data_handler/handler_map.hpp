abd141c33786987f66fba0efb9499c84ed7cc7d3
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-14 15:19:13 -0600

    feat: Connect different handlers to different ids

---
+++ include/cool_serial/data_handler/handler_map.hpp
@@ -0,0 +1,13 @@
+#include <unordered_map>
+
+#include "cool_serial/data_handler/idata_handler.hpp"
+namespace coolSerial
+{
+    /**
+     * This is used to connect IDataHandlers to a byte protocol
+     *
+     * WARNING: This does NOT check for bounds. Max protocol number is
+     * 256. (Ids range from 0 to 255)
+     */
+    using HandlerMap = std::unordered_map<coolSerial::Byte, std::reference_wrapper<IDataHandler>>;
+}
