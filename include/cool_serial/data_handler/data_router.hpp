783e216daae9ddfae0d4e452f3d5067bf22c5756
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-14 16:31:54 -0600

    fix: Proper access
    operator[] is just a bit finicky

--- include/cool_serial/data_handler/data_router.hpp
+++ include/cool_serial/data_handler/data_router.hpp
@@ -2,6 +2,7 @@
 #define COOL_SERIAL_DATA_HANDLER_DATA_ROUTER_HPP
 
 #include "cool_serial/data_handler/handler_map.hpp"
+#include "cool_serial/data_handler/idata_handler.hpp"
 #include "cool_serial/dynamic_parser/data_found_listener.hpp"
 
 namespace coolSerial
@@ -23,9 +24,19 @@ namespace coolSerial
         DataRouter() : handlerMap_{}
         {}
 
+        /**
+         * Change a handler.
+         *
+         * If the handler has not been set, it will add a new map
+         */
+        void setMapping(Byte dataType, IDataHandler& handler)
+        {
+            handlerMap_.at(dataType) = handler;
+        }
+
         void dataFound(const CoolMessageData& data)
         {
-            IDataHandler& handler{handlerMap_[data.dataType].get()};
+            IDataHandler& handler{handlerMap_.at(data.dataType).get()};
             handler.handleData(data.data);
         }
     private:
