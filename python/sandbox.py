5a689c0064f01dd18524511534517137bf608d0f
Jake <jakewolf0825@gmail.com>
2025-11-22 14:43:04 -0600

    Created Python folder as project foundation.

---
+++ python/sandbox.py
@@ -0,0 +1,12 @@
+# Conduct module testing here or add notes & comments for future
+
+# Bytes literal 101 (please correct if I'm wrong) :
+
+# 0x is the standard prefix for the Python parser
+# EXAMPLE: >>> 0x84 will display 132
+
+# \x (escape sequence x) is the standard prefix for bytes literal
+# EXAMPLE: >>> b'/x48' will display 'H'
+
+# They are technically the same since machine only reads binary (0s & 1s)
+# However, note that the MCB only expects bytes literal prefix (\x)
--- test/sandbox_py/test.py
+++
@@ -1,4 +0,0 @@
-# Starting point for the project
-# Feel free to conduct any form of module testing here
-# Feel free to create new .py files for further testing under sandbox_py 
-
